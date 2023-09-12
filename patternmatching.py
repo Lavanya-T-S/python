23995f5357af176dc8f6293f11ec087dfc96e2f7
Lavanya-T-S <115984634+Lavanya-T-S@users.noreply.github.com>
2023-09-12 16:24:50 +0530

    Patternmatching

---
+++ patternmatching.py
@@ -0,0 +1,27 @@
+
+for i in range(4):
+    for j in range(4):
+        print("#",end="")
+    print()
+
+
+for i in range(4):
+    for j in range(4-i):
+        print("#",end="")
+    print()
+
+
+
+#for i in range(4): #0123
+ #   for j in range(4-i):
+         #print(j+i,end="")#23
+    #print()
+
+
+
+
+
+
+
+
+
