fd8a11c12c113d8514d36b92526f6f4065965b2f
Lavanya-T-S <115984634+Lavanya-T-S@users.noreply.github.com>
2023-09-14 13:33:29 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,9 @@
+def greet_user(first_name,Last_name):
+    first_name=input("enter a name")
+    Last_name=input("enter a last_name")
+    print(f'hello {first_name,Last_name} ')
+    print('i am fyn')
+
+print("start")
+greet_user(first_name=" ",Last_name=" ")
+print("stop")
