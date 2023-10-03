cda3c85eabd2d659b31b53cfb5d84eed417903c8
Lavanya-T-S <115984634+Lavanya-T-S@users.noreply.github.com>
2023-10-03 15:59:23 +0530

    Fibonacci series

---
+++ fibonacci series.py
@@ -0,0 +1,21 @@
+def fib(n):
+    a=0
+    b=1
+    if fib==1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+    for i in (2,fib):
+        c=a+b
+        a=b
+        b=c
+        print(c)
+
+fib(input('enter a number'))
+
+
+
+
+
+
