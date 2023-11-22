91a5e3b8d396b80cf59b849d541103f378d5f7ea
Lavanya-T-S <115984634+Lavanya-T-S@users.noreply.github.com>
2023-11-22 17:16:52 +0530

    object of innr is created outside of outercls

--- innerclass.py
+++ innerclass.py
@@ -2,19 +2,19 @@ class Student:
     def __init__(self,name,rollno):
         self.name=name
         self.rollno=rollno
-        self.lap=self.Laptop()##object of inner class is created inside the outer class
+
 
 
     def show(self):
         print(self.name,self.rollno)
-        self.lap.show()
+
 
 
     class Laptop:
         def __init__(self):
-            self.brand = 'HP'
-            self.cpu = 'i5'
-            self.ram =8
+            self.brand=brand
+            self.cpu=cpu
+            self.ram=ram
 
         def show(self):
             print(self.brand ,self.cpu,self.ram)
@@ -24,4 +24,6 @@ class Student:
 s1=Student("jas",22)
 s2=Student("la",24)
 s1.show()
-lap1=s1.lap#object calling
+l1=Student.Laptop('Hp','i5',8)#object of inner class is created at outside of outer class
+l1.show()
+
